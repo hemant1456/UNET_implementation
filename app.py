642f98503364ee01055452d60a138715f90ea198
hemant <hkbhambhu@gmail.com>
2024-03-18 04:58:45 +0530

    added the gradio app code

---
+++ app.py
@@ -0,0 +1,24 @@
+import gradio as gr
+import torch
+from torchvision import transforms
+
+from UNET_model import UNET
+from utils import segment_image
+
+model = UNET(3, 64)
+model.load_state_dict(torch.load("./unet_model.pth"))
+
+
+def image_segment(image):
+    image_transforms = transforms.Compose([ transforms.Resize((224,224)),transforms.ToTensor()])
+    transformed_image = image_transforms(image)
+    output = model(transformed_image.unsqueeze(0))
+    pred = output.argmax(dim=1)
+    image_out = segment_image(transformed_image, pred[0])
+    to_pil_image = transforms.ToPILImage()
+    return to_pil_image(image_out)
+
+
+app = gr.Interface(image_segment, inputs=[gr.Image(type="pil", height=224, width=224)], outputs=[gr.Image(type="pil", height=224, width=224)], examples=['./examples/Abyssinian_2.jpg','./examples/american_bulldog_101.jpg',
+                                                                                              './examples/american_pit_bull_terrier_184.jpg'])
+app.launch()
