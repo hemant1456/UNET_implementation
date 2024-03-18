702d44ddacb024c7971cc48e317e53b884aa94a2
hemant <hkbhambhu@gmail.com>
2024-03-19 04:59:42 +0530

    provided options for downsizing and upsampling layers

--- decoder.py
+++ decoder.py
@@ -3,13 +3,16 @@ import torch.nn.functional as F
 import torch
 
 class DecoderBlock(nn.Module):
-    def __init__(self, in_channels, num_filters):
+    def __init__(self, in_channels, num_filters,upsample_type="transpose"):
         super().__init__()
-        self.conv_transpose = nn.ConvTranspose2d(in_channels, num_filters, kernel_size=3, stride=2, padding=1, output_padding=1)
+        if upsample_type=="transpose":
+            self.up = nn.ConvTranspose2d(in_channels, num_filters, kernel_size=3, stride=2, padding=1, output_padding=1)
+        elif upsample_type=="upsample":
+            self.up= nn.Upsample(scale_factor=2, mode="nearest")
         self.conv1 = nn.Conv2d(num_filters*2, num_filters, 3, 1, 1)
         self.conv2 = nn.Conv2d(num_filters, num_filters, 3, 1, 1)
     def forward(self,x, prev_layer):
-        x = self.conv_transpose(x)
+        x = self.up(x)
         x = torch.cat([x, prev_layer], dim=1)
         x = F.relu(self.conv2(F.relu(self.conv1(x))))
         return x
--- encoder.py
+++ encoder.py
@@ -2,11 +2,14 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 class EncoderBlock(nn.Module):
-    def __init__(self, in_channels, num_filters, to_pool=True):
+    def __init__(self, in_channels, num_filters, to_pool=True, pool_type="strided"):
         super().__init__()
         self.conv1 = nn.Conv2d(in_channels, num_filters, 3, 1, 1)
         self.conv2 = nn.Conv2d(num_filters, num_filters, 3, 1, 1)
-        self.max_pool = nn.MaxPool2d(2,2)
+        if to_pool and pool_type=="strided":
+            self.pool = nn.Conv2d(num_filters, num_filters, 3, 2, 1)
+        elif to_pool and pool_type=="max_pool":
+            self.pool = nn.MaxPool2d(2,2)
         self.batch_norm1 = nn.BatchNorm2d(num_filters)
         self.batch_norm2 = nn.BatchNorm2d(num_filters)
         self.to_pool =to_pool
@@ -14,5 +17,5 @@ class EncoderBlock(nn.Module):
         x = self.batch_norm1(F.relu(self.conv1(x)))
         x = self.batch_norm2(F.relu(self.conv2(x)))
         if self.to_pool:
-            x = self.max_pool(x)                  
+            x = self.pool(x)                  
         return x
