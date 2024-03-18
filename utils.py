bca4c1a2e8472486fc5e94a7caefa87bd3ba5b6a
hemant <hkbhambhu@gmail.com>
2024-03-19 05:02:15 +0530

    added dice loss

--- utils.py
+++ utils.py
@@ -1,4 +1,6 @@
 from einops import rearrange
+import torch
+
 def segment_image_oxford(image, mask):
     mask = mask.unsqueeze(0).repeat(3,1,1)
     image[mask==1]=0
@@ -38,3 +40,15 @@ def plot_prediction(image, label, pred, data_name):
     plt.imshow(rearrange(pred_seg, "c h w -> h w c"), cmap="gray")
     plt.title("Predicted_Image_segmented")
     plt.axis("off")
+
+import torch.nn.functional as F
+def dice_loss(output, target, num_classes):
+    pred = F.softmax(output, dim=1)
+    dice_loss = 0
+    target = rearrange(torch.nn.functional.one_hot(target,num_classes),"b h w c -> b c h w")
+    classes_present = torch.sum(target,dim = (2,3))>0
+    intersection = torch.sum(pred * target, dim= (2,3)) # [N,C]
+    union = torch.sum(pred, dim=(2,3)) + torch.sum(target, dim=(2,3))
+    dice_cofficient = 2 * intersection / (union + 1e-5)
+    dice_loss = (1 - dice_cofficient) * classes_present
+    return dice_loss.sum()/ classes_present.sum()
