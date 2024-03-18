7081a718b5d81df71f16fe4447712c4ed4a9e705
hemant <hkbhambhu@gmail.com>
2024-03-19 04:58:03 +0530

    removed albumentation to keep the code simple

--- dataloader.py
+++ dataloader.py
@@ -8,18 +8,20 @@ import numpy as np
 
 
 class data_class(Dataset):
-    def __init__(self,data, image_transforms, name):
+    def __init__(self,data, image_transforms, mask_transforms,name):
         super().__init__()
         self.data = data
         self.image_transforms = image_transforms
-        self.name = name
+        self.mask_transforms = mask_transforms
+        self.name=name
     def __len__(self):
         return len(self.data)
     def __getitem__(self, index):
         image, mask = self.data[index]
-        transformed_img_mask= self.image_transforms(image= np.array(image), mask=np.array(mask))
-        image_transformed = transformed_img_mask["image"]
-        mask_transformed = transformed_img_mask["mask"]
+        image_resized = TF.resize(image, size=[224,224],interpolation= TF.InterpolationMode.BILINEAR)
+        mask_resized = TF.resize(mask, size=[224,224],interpolation= TF.InterpolationMode.NEAREST)
+        image_transformed= self.image_transforms(image_resized)
+        mask_transformed = self.mask_transforms(mask_resized)
         if self.name=="oxford":
             mask_transformed -=1  # as our classes as 1,2,3 but pytorch needs as 0,1,2
         elif self.name=="voc":
@@ -27,13 +29,10 @@ class data_class(Dataset):
         return image_transformed, mask_transformed.squeeze(0).long()
 
 
-def get_dataloader(data="oxford"):
+def get_dataloader(data="oxford", batch_size=32):
 
-    image_transforms = A.Compose([A.Resize(height=224,width=224),
-                                  A.HorizontalFlip(p=0.5),
-                                  A.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225)),
-                                  A.ShiftScaleRotate(),
-                                  ToTensorV2()])
+    image_transforms = transforms.Compose([ transforms.ToTensor()])
+    mask_transforms = transforms.Compose([transforms.PILToTensor()])
     
     if data=="oxford":
         train_data = datasets.OxfordIIITPet("./oxford", download=True, split="trainval",target_types="segmentation")
@@ -42,10 +41,9 @@ def get_dataloader(data="oxford"):
         train_data= datasets.VOCSegmentation("./voc",image_set="train",year="2012",download=True)
         test_data= datasets.VOCSegmentation("./voc",image_set="val",year="2012",download=True)
 
+    train_data = data_class(train_data, image_transforms, mask_transforms, name=data)
+    test_data = data_class(test_data, image_transforms, mask_transforms,name=data)
 
-    train_data = data_class(train_data, image_transforms, name=data)
-    test_data = data_class(test_data, image_transforms, name=data)
-
-    train_loader = DataLoader(train_data, batch_size=12, shuffle=True, pin_memory=False, num_workers=5)
-    test_loader = DataLoader(test_data, batch_size=12, shuffle=True, pin_memory=False, num_workers=2) 
+    train_loader = DataLoader(train_data, batch_size=batch_size, shuffle=True, pin_memory=False, num_workers=5)
+    test_loader = DataLoader(test_data, batch_size=batch_size, shuffle=True, pin_memory=False, num_workers=2) 
     return train_loader, test_loader
